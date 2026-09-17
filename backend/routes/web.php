61e1a1766dfac814ba601281574d5b2215d2dfa1
Rangga Pratama Aditya <237360899+RanggaPratamaAditya@users.noreply.github.com>
2026-09-17 20:27:47 +0700

    Update project terbaru

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tickets', [TicketController::class, 'index'])
+    ->name('tickets.index');
+
+Route::get('/tickets/{ticket}', [TicketController::class, 'show'])
+    ->whereNumber('ticket')
+    ->name('tickets.show');
+
+Route::get('/api/tickets/{ticket}', [TicketController::class, 'showJson'])
+    ->whereNumber('ticket')
+    ->name('tickets.show-json');
